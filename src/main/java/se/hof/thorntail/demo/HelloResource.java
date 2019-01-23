614f7d2864782faff9b9f3f7d8668458aa5d522b
Joel Hofgren <joel@Joels-MacBook-Pro.local>
2019-01-23 23:54:21 +0100

    Simple thorntail.io example app

---
+++ src/main/java/se/hof/thorntail/demo/HelloResource.java
@@ -0,0 +1,21 @@
+package se.hof.thorntail.demo;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+@Path("/hello")
+public class HelloResource {
+
+    @GET
+    @Produces("text/plain")
+    public String hello() {
+        return "Hello!";
+    }
+
+    @GET
+    @Produces("application/json")
+    public String helloJson() {
+        return "{ \"greeting\": \"Hello!\" }";
+    }
+}
---
+++ src/main/java/se/hof/thorntail/demo/SimpleRestApplication.java
@@ -0,0 +1,9 @@
+package se.hof.thorntail.demo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class SimpleRestApplication extends Application {
+
+}
